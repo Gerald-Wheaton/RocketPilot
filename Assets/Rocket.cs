4ee0ad7f80a3ca0abde155b8f0d9866aacf50d54
Gerald-Wheaton <geraldwheaton2000@gmail.com>
2020-04-11 11:48:27 -0400

    checkpoint

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,60 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rocketThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
     Rigidbody rigidBody;
-    AudioSource thrustSound;
+    AudioSource audioSource;
+
+    enum State{Alive, Transcending, Dying};
+    State currentState;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         rigidBody = GetComponent<Rigidbody>();
-        thrustSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
-        Thrust();
-        MotionDirection();
+        if (currentState != State.Dying) 
+        {
+            RespondToThrustInput();
+            RespondToRotationInput();
+        }
     }
 
-    void OnCollisionEnter(Collision collision) {
+    void OnCollisionEnter(Collision collision) 
+    {
+        if (currentState != State.Alive) { //ignore collisions if dead
+            return;
+        }
+
         switch(collision.gameObject.tag) { //reads gameObject's tag
             case "Friendly":
                 print("Ok");
+                currentState = State.Alive;
+                break;
+            case "Finish":
+                StartSuccessSequence();
                 break;
             case "Enemy":
-                print("Dead");
-                Start();
+                StartDeathSequence();
                 break;
         }
     }
+
+    private void StartSuccessSequence() 
+    {
+        currentState = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextScene", 3f); //waits 1 second before calling function. Invoke requires a string
+    }
+
+    private void StartDeathSequence()
+    {
+        currentState = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 3f);
+    }
         
-    void Thrust() {
-        if (Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!thrustSound.isPlaying) { //prevents sound from layering
-                thrustSound.Play();
-            }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0); //level one
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); //level two
+    }
+
+    void RespondToThrustInput() 
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
         }
-        else {
-            thrustSound.Stop();
+        else 
+        {
+            print("Stopped Sound");
+            audioSource.Stop();
         }
     }
-    void MotionDirection() {
 
+    void ApplyThrust() 
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) //prevents sound from layering
+        { 
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    void RespondToRotationInput()
+    {
         rigidBody.freezeRotation = true; //take manual control of the rotation
 
         float rotationThisFrame = rocketThrust * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.A)) {
-           
+        if (Input.GetKey(KeyCode.A)) 
+        {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
-        else if (Input.GetKey(KeyCode.D)) {
+        else if (Input.GetKey(KeyCode.D)) 
+        {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
