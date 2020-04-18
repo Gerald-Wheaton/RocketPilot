108cdbe68d3edd0e77862b3b9ef279be469f80df
Gerald-Wheaton <geraldwheaton2000@gmail.com>
2020-04-18 19:45:29 -0400

    checkpoint:

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -10,6 +10,7 @@ public class Rocket : MonoBehaviour
     [SerializeField] AudioClip mainEngine;
     [SerializeField] AudioClip success;
     [SerializeField] AudioClip death;
+    [Range(2, 5)][SerializeFiled] int startingLives;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
